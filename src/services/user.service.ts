3e62f30ee51b1960040674f3092e10cc81acae01
Vladimir <dev.smartit@gmail.com>
2023-03-19 20:59:28 +0200

    create commDto and update userService(func create)

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { UserModel } from 'src/models/user.model';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @InjectModel(UserModel)
+    private userModel: typeof UserModel,
+  ) {}
+
+  async create(email: string, nUsername: string): Promise<UserModel> {
+    const findUser = await this.userModel.findOne({ where: { email } });
+    if (findUser && findUser.username !== nUsername) {
+      await findUser.update({ username: nUsername });
+      return findUser;
+    } else if (findUser && findUser.username === nUsername) {
+      return findUser;
+    } else {
+      const user = await this.userModel.create({ email, username: nUsername });
+      return user;
+    }
+  }
 }
