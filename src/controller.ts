473744cb13e82c89bd393e92bfd7ed982c70e88a
Vladimir <dev.smartit@gmail.com>
2023-03-20 16:11:08 +0200

    comment create

--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Param,
+  Query,
+  UseInterceptors,
+  ParseFilePipeBuilder,
+  UploadedFile,
+  HttpStatus,
+  ParseFilePipe,
+} from '@nestjs/common';
+import { Express } from 'express';
+import { CommentService } from './services/comment.service';
+import { FileService } from './services/file.service';
+import { UserService } from './services/user.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateCommentDto } from './dto/createCommentDto';
 
 @Controller('/comments')
-export class CommentController {}
+export class CommentController {
+  constructor(
+    private userService: UserService,
+    private commentService: CommentService,
+    private fileService: FileService,
+  ) {}
+
+  @Post('/create')
+  @UseInterceptors(FileInterceptor('file'))
+  async createComment(
+    @Body() data: CreateCommentDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'text/plain',
+        })
+        .addMaxSizeValidator({
+          maxSize: 100000,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file?: Express.Multer.File,
+    @Query('parentId') parentId?: number,
+  ) {
+    const user = await this.userService.create(data.email, data.username);
+    const comment = await this.commentService.createComment(
+      data.text,
+      user.id,
+      parentId,
+    );
+    const finishedFile = await this.fileService.create(file, comment.id);
+    const commentForRes = {
+      username: user.username,
+      email: user.email,
+      text: comment.text,
+      createdAt: comment.createdAt,
+      file: finishedFile.name,
+    };
+    return commentForRes;
+  }
+}
--- src/dto/createCommentDto.ts
+++ src/dto/createCommentDto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateCommentDto {
-  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @IsNotEmpty()
